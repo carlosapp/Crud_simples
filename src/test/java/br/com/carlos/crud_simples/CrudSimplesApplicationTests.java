fc4d349c1418c5638841a70ffc2ebad99c823878
Dunga <josecarlosdasilva18@gmail.com>
2025-07-10 20:47:40 -0300

    primeiro commit

---
+++ src/test/java/br/com/carlos/crud_simples/CrudSimplesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.carlos.crud_simples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudSimplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
