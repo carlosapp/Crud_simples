314f8511c1ca90985b205ed464c092a8b01d2a8a
Dunga <josecarlosdasilva18@gmail.com>
2025-07-10 21:09:22 -0300

    criacao de model pessoa

---
+++ src/main/java/br/com/carlos/crud_simples/model/Pessoa.java
@@ -0,0 +1,37 @@
+package br.com.carlos.crud_simples.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name ="Pessoa")
+public class Pessoa {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    @Column(nullable = false, length = 150)
+    private String nome;
+    @Column(nullable = true, length = 11)
+    private String cpf;
+    @Column(nullable = false)
+    private boolean ativo;
+
+    public Pessoa() {
+    }
+
+    public Pessoa(String nome, String cpf, boolean ativo) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.ativo = ativo;
+    }
+
+
+
+
+
+
+
+}
